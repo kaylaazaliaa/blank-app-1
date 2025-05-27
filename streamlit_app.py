6ebf1d22d437e23f4c009f3a74ab06e007f704d0
kaylaazaliaa <kaylazharifah680@gmail.com>
2025-05-27 08:00:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title("selamat datang di website kaylaazalia")
 st.write(
     "ngoding seru bersama kayla."
 )
-st.image(802937D8-A9EA-4B5B-B149-6D982E3E804B.jpeg, width=500)
+st.image("802937D8-A9EA-4B5B-B149-6D982E3E804B.jpeg",width=500)
